28e335820ef4cbf7a6b5384ab7a2bda7a44c1cb7
abhisivasailam <cldfusion@gmail.com>
2016-01-19 22:18:51 -0800

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,5 @@
-# config/unicorn.rb
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 1)
-timeout 25
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 2)
+timeout 60
 preload_app true
 
 before_fork do |server, worker|
@@ -8,19 +7,10 @@ before_fork do |server, worker|
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
-
-  defined?(ActiveRecord::Base) and 
-    ActiveRecord::Base.connection.disconnect!
-
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-
-  defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-
-  $redis and $redis.client.reconnect
 end
